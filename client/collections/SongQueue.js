8043a1d69eb7aa9260b95674e2c1813eb1e89f32
ACP31 <aaronp48@gmail.com>
2015-10-22 14:11:06 -0500

    working version

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,19 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    //this.playFirst();
+  },
+
+  playFirst: function() {
+    if(this.length !== 0){
+      this.at(0).play();
+    }
   }
 
+
+
 });
+
+//set the first item in the Q. to current song
+  //have an event that checks for the current song
+    //that event will then play the current song
